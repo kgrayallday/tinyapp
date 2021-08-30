208b550a14753a509a62a7105a7bfac4124c79e7
kyl graym <kylemaxgray@gmail.com>
2021-08-30 21:14:16 +0000

    following LHL tut

--- express_server.js
+++ express_server.js
@@ -7,15 +7,29 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.render('/pages/index');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = {urls: urlDatabase };
+  res.render('urls_index', templateVars);
+  const urls = [
+    { "b2xVn2": "http://www.lighthouselabs.ca"},
+    {"9sm5xK": "http://www.google.com"}
+  ];
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
