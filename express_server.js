286ac981eb38da0bcd6af440de51558223fd2e65
kyl graym <kylemaxgray@gmail.com>
2021-08-31 02:41:19 +0000

    created urls_new and included in 'express_server'

--- express_server.js
+++ express_server.js
@@ -22,6 +22,10 @@ app.get('/urls', (req, res) => {
   ];
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL/* What goes here? */ };
   res.render("urls_show", templateVars);
@@ -30,6 +34,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
 
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
