47685949205b16e8cfd9a8d9fc2447cbb3185428
kyl graym <kylemaxgray@gmail.com>
2021-08-31 04:22:12 +0000

    wrap up url shortening (part 2)

--- express_server.js
+++ express_server.js
@@ -20,9 +20,15 @@ app.get("/", (req, res) => {
   res.render('/pages/index');
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase.res.params.shortURL = res.params.longURL;
+  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
 });
 
 app.get('/urls', (req, res) => {
