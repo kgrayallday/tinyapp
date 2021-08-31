53043feac6210ade28aae57524c657469c70faad
kyl graym <kylemaxgray@gmail.com>
2021-08-31 01:34:30 +0000

    update header name, added new partials, and other updates

--- express_server.js
+++ express_server.js
@@ -22,6 +22,11 @@ app.get('/urls', (req, res) => {
   ];
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL/* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
