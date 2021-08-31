ad8a304c639b9d3a7dfed7e3f2c1d60a6e780c42
kyl graym <kylemaxgray@gmail.com>
2021-08-31 03:37:14 +0000

    wrap up of url shortening (part 1)

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString(length = 6) {
+  return Math.random().toString(16).substr(2, length);
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -9,8 +13,16 @@ const urlDatabase = {
 
 app.set('view engine', 'ejs');
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.render('/pages/index');
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get('/urls', (req, res) => {
