cec7bcbec74330f8457d7b12a07425a585c0083d
kyl graym <kylemaxgray@gmail.com>
2021-08-31 19:32:50 +0000

    updated comments

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 function generateRandomString(length = 6) {
   return Math.random().toString(16).substr(2, length);
@@ -16,24 +16,24 @@ app.set('view engine', 'ejs');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { // render /pages/index when root is visited
   res.render('/pages/index');
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+app.get("/u/:shortURL", (req, res) => {              // takes the shortUrl link request
+  const longURL = urlDatabase[req.params.shortURL];  // and links to the actual(longUrl) by referencing 
+  res.redirect(longURL);                             // the urlDatabase variables based on the :shortUrl passed
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  urlDatabase.res.params.shortURL = res.params.longURL;
-  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
+  console.log(req.body);                                // Log the POST request body to the console when visiting 8080/urls
+  urlDatabase.res.params.shortURL = res.params.longURL; // insert new shortUrl : longUrl key-value pairs to our urlDatabase obj
+  res.redirect(`/urls/${shortURL}`);                    // redirect to specific shorturl key site
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase };
-  res.render('urls_index', templateVars);
+  const templateVars = {urls: urlDatabase }; // passing the object as is so we can iterate through keys later
+  res.render('urls_index', templateVars);    // render urls_index with templateVars data/variables
   const urls = [
     { "b2xVn2": "http://www.lighthouselabs.ca"},
     {"9sm5xK": "http://www.google.com"}
@@ -41,16 +41,16 @@ app.get('/urls', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-});
+  res.render('urls_new');           // urls_new really only contains an include of partial header
+});                                 // so no need for templateVars
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL/* What goes here? */ };
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
   res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`tiny app listening on port ${PORT}!`);
 });
 
 
