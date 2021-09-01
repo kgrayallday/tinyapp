1fc7d4b4f2a27ae6606db7a9a265bf8f5cddabe0
kyl graym <kylemaxgray@gmail.com>
2021-09-01 04:41:43 +0000

    login functionality locked in

--- express_server.js
+++ express_server.js
@@ -16,45 +16,82 @@ app.set('view engine', 'ejs');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => { // render /pages/index when root is visited
-  res.render('/pages/index');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.get('/', (req, res) => { // render /pages/index when root is visited (NOT WORKING)
+  console.log('Cookies: ', req.cookies)
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
+  res.render('./pages/index', templateVars);
 });
 
-// Delete POST /urls/:shortUrl/delete
-app.post("/urls/:shortUrl/delete", (req, res) => {
-  const shortUrl = req.params.shortUrl;
-  console.log('short url passed after delete click', shortUrl);
-  delete urlDatabase[shortUrl];
+// Login POST
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
+// Delete POST /urls/:shortURL/delete
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  console.log('short url passed after delete click', shortURL);
+  delete urlDatabase[shortURL];
   res.redirect('/urls'); // on Delete("Destroy") redirects back to url index
 });
 
-app.get("/u/:shortURL", (req, res) => {              // takes the shortUrl link request
-  const longURL = urlDatabase[req.params.shortURL];  // and links to the actual(longUrl) by referencing 
-  res.redirect(longURL);                             // the urlDatabase variables based on the :shortUrl passed
+// Update a URL with POST
+app.post("/urls/:shortURL", (req, res) => {
+  const longURL = req.body.longURL; // NEED TO FIX THIS ID IS NOT A PROPERTY OF PARAMS
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls');
 });
 
-// Add a url with POST 
+// Add a new url with POST 
 app.post("/urls", (req, res) => {
   console.log(req.body);                                // Log the POST request body to the console when visiting 8080/urls
-  urlDatabase.res.params.shortURL = res.params.longURL; // insert new shortUrl : longUrl key-value pairs to our urlDatabase obj
-  res.redirect(`/urls/${shortURL}`);                    // redirect to specific shorturl key site
+  const randId = generateRandomString();
+  urlDatabase[randId] = req.body.longURL; // insert new shortURL : longURL key-value pairs to our urlDatabase obj
+  res.redirect(`/urls/${randId}`);                    // redirect to specific shortURL key site
 });
+
+app.get("/u/:shortURL", (req, res) => {              // takes the shortURL link request
+  const longURL = urlDatabase[req.params.shortURL];  // and links to the actual(longURL) by referencing 
+  res.redirect(longURL);                             // the urlDatabase variables based on the :shortURL passed
+});
+
+
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase }; // passing the object as is so we can iterate through keys later
+  // console.log('username tmpVars from get /urls', res.cookies["username"]);
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render('urls_index', templateVars);    // render urls_index with templateVars data/variables
-  const urls = [
-    { "b2xVn2": "http://www.lighthouselabs.ca"},
-    {"9sm5xK": "http://www.google.com"}
-  ];
 });
 
+// Create new tiny URLs
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');           // urls_new really only contains an include of partial header
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
+  res.render('urls_new', templateVars);           // urls_new really only contains an include of partial header
 });                                 // so no need for templateVars
 
+// brings us to each urls specific page
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
+  console.log(req.params.longURL)
   res.render("urls_show", templateVars);
 });
 
@@ -65,8 +102,4 @@ app.listen(PORT, () => {
 
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
 // });
